a821c6a48fd9dacfc51e1c8dce3e4ef5b0b69d81
G234i <georgiivanov22@gmail.com>
2026-05-26 23:31:19 +0300

    Moved img logic to a seperate component, main page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,24 @@
-import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import { fetchRandomCat } from "./services/catFetchService";
-import { DateTimeHelper } from "./helpers/dateTimeHelper";
+import { ImageContainerComp } from "./componnents/ImageContainerComp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const [catImageUrl, setCatImageUrl] = useState<string | null>(null);
-  const [timestamp, setTimestamp] = useState<number | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    return () => {
-      if (catImageUrl) {
-        URL.revokeObjectURL(catImageUrl);
-      }
-    };
-  }, [catImageUrl]);
-
-  const handleFetchCat = async () => {
-    setLoading(true);
-    setError(null);
-
-    const result = await fetchRandomCat();
-    setLoading(false);
-
-    if (result.success && result.imageUrl) {
-      setCatImageUrl(result.imageUrl);
-      setTimestamp(result.time ?? null);
-    } else {
-      setError(result.error ?? "Unable to load a cat image. Please try again.");
-      setCatImageUrl(null);
-    }
-  };
+  const githubUrl = "https://github.com/georgi359/WebExtensionTask";
 
   return (
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={heroImg} className="base" width="75" height="80" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Chrome Extension Assignment</h1>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-        <button
-          type="button"
-          className="counter"
-          onClick={handleFetchCat}
-          disabled={loading}
-          style={{ marginLeft: "1rem" }}
-        >
-          {loading ? "Loading cat..." : "Fetch random cat"}
-        </button>
+
+        <ImageContainerComp />
       </section>
-
-      <div className="ticks"></div>
-
-      {error && (
-        <section id="error-message">
-          <p style={{ color: "red" }}>{error}</p>
-        </section>
-      )}
-
-      {catImageUrl && timestamp && (
-        <section id="cat-preview">
-          <h2>Random Cat: {DateTimeHelper.formatFullTimestamp(timestamp)}</h2>
-          <img
-            src={catImageUrl}
-            alt="Random cat"
-            style={{ maxWidth: "100%", borderRadius: "12px" }}
-          />
-        </section>
-      )}
 
       <section id="next-steps">
         <div id="social">
@@ -97,10 +28,7 @@ function App() {
           <h2>Project GitHub</h2>
           <ul>
             <li>
-              <a
-                href="https://github.com/georgi359/WebExtensionTask"
-                target="_blank"
-              >
+              <a href={githubUrl} target="_blank">
                 <svg
                   className="button-icon"
                   role="presentation"
---
+++ src/componnents/ImageContainerComp.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { fetchRandomCat } from "../services/catFetchService";
+import { DateTimeHelper } from "../helpers/dateTimeHelper";
+// import "../App.css";
+
+export function ImageContainerComp() {
+  const [catImageUrl, setCatImageUrl] = useState<string | null>(null);
+  const [timestamp, setTimestamp] = useState<number | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (catImageUrl) {
+        URL.revokeObjectURL(catImageUrl);
+      }
+    };
+  }, [catImageUrl]);
+
+  const handleFetchCat = async () => {
+    setLoading(true);
+    setError(null);
+
+    const result = await fetchRandomCat();
+    setLoading(false);
+
+    if (result.success && result.imageUrl) {
+      setCatImageUrl(result.imageUrl);
+      setTimestamp(result.time ?? null);
+    } else {
+      setError(result.error ?? "Unable to load a cat image. Please try again.");
+      setCatImageUrl(null);
+    }
+  };
+
+  return (
+    <section>
+      <button
+        type="button"
+        className="button"
+        onClick={handleFetchCat}
+        disabled={loading}
+        style={{ marginLeft: "1rem" }}
+      >
+        {loading ? "Loading cat..." : "Fetch random cat"}
+      </button>
+
+      {error && (
+        <section id="error-message">
+          <p style={{ color: "red" }}>{error}</p>
+        </section>
+      )}
+
+      {catImageUrl && timestamp && (
+        <section id="cat-preview">
+          <h2>Random Cat: {DateTimeHelper.formatFullTimestamp(timestamp)}</h2>
+          <img className="cat-image" src={catImageUrl} alt="Random cat" />
+        </section>
+      )}
+    </section>
+  );
+}
